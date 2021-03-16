195f0a83af19759fb854b1b32a7f706159e78450
AmarMusic91 <music.amar@hotmail.com>
2021-03-16 21:26:44 +0100

    ispravka

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,8 +1,8 @@
 function logicalAndOperator(num) {
-    if(num <= 80 && num >= 40) {
-        return "Yes";
-    }
-    return "No";
+  if (num <= 80 && num >= 40) {
+    return "Yes";
+  }
+  return "No";
 }
 console.log(logicalAndOperator(0));
 console.log(logicalAndOperator(24));
