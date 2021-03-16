b6c00afc45ee793170e87dc7798eb54ac56a7254
AmarMusic91 <music.amar@hotmail.com>
2021-03-16 20:08:39 +0100

    ispravka - sve

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,8 +1,8 @@
 function myFunction(myParameter) {
-    if(myParameter) {
-        return "The parameter is true!";
-    }
-        return "The parameter is false!";
+  if (myParameter) {
+    return "The parameter is true!";
+  }
+  return "The parameter is false!";
 }
 myFunction(true);
 myFunction(false);
