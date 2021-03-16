ecfc19f2b182cd19c51aa74ec6412df5b6bd4f67
AmarMusic91 <music.amar@hotmail.com>
2021-03-17 00:53:10 +0100

    ispravka

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -27,7 +27,7 @@ function myForLoop1() {
   console.log(myForLoop1());
   console.log(myForLoop2());
   
-  //Only change code above this line
+
   module.exports = {
     myForLoop1,
     myForLoop2,
